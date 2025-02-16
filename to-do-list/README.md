6340f4c36a3328a4e6f512f8a22c5cdfc59f1ef2
Anton Schukin <antonschukin@gmail.com>
2025-02-16 16:48:20 +0200

    project preparation

---
+++ to-do-list/README.md
@@ -0,0 +1,3 @@
+# Day 1 - To-Do List
+
+Link: https://antonioooooooooooo.github.io/to-do-list/
---
+++ to-do-list/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div>Hello World</div>
+  </main>
+</template>
+
+<style scoped>
+</style>
